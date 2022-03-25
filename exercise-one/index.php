e1a412f0cbe54f3e8acf00e7a11e1078a48be511
Nathalia-Gomez <Nathalia_Gomez_Jimenez@gmail.com>
2022-03-24 19:28:06 -0600

    u

--- exercise-one/index.php
+++ exercise-one/index.php
@@ -1,8 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 
-    require_once "clase.php";
-    $casa1=new Casa("Juan", "Residencial", 123);
-    $casa1->imprimirImpuesto();
-    echo "<br>".$casa1->getPropietario()."<br>";
+require_once "clase.php";
+$casa1=new Casa("Juan", "Residencial", 123);
+$casa1->imprimirImpuesto();
+echo "<br>".$casa1->getPropietario()."<br>";
 
 ?>
+</body>
+</html>
+
